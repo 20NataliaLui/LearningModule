48f1e85a7259339b0bc93d8a7fbc29a4aff8f78f
Natalia Lui <47930379+20NataliaLui@users.noreply.github.com>
2021-04-06 16:40:29 -0400

    Update LED_Rectangle.py

--- LED_Rectangle.py
+++ LED_Rectangle.py
@@ -30,7 +30,7 @@ GPIO.setup(c_pin, GPIO.OUT)
 GPIO.setup(latch_pin, GPIO.OUT)
 GPIO.setup(oe_pin, GPIO.OUT)
 
-screen = [[0 for x in xrange(32)] for x in xrange(16)]
+screen = [[0 for x in xrange(16)] for x in xrange(16)]
 
 def clock():
     GPIO.output(clock_pin, 1)
